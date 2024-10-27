4550d3dcd7a822362534c97e7163ab3f40889242
rasw0001 <rasw0001@student.monash.edu>
2024-10-27 13:03:25 +1100

    Completed Part B as well

---
+++ R/launchApp.R
@@ -0,0 +1,8 @@
+#' Launch the Shiny App
+#'
+#' This function launches the Shiny application contained in the package.
+#' @return No return value, called for side effects.
+#' @export
+launchApp <- function() {
+    shiny::runApp("C:/Users/risha/assignment4shiny/inst/app/App.R")
+}
--- launchApp.R
+++ launchApp.R
@@ -1,15 +1,7 @@
-#' @title Launch the Shiny App
-#' @description This function launches the Shiny application contained in the package.
-#' @return No return value, called for side effects.
+#' Launch the Shiny App
+#'
 #' @export
 launchApp <- function() {
-    shiny::runApp(system.file("app", package = "assignment4shiny"))
+    print("Testing launchApp function")
 }
 
-#' Launch the Fuel Economy Shiny App
-#'
-#' This function launches a Shiny app for exploring the mpg dataset.
-#' @export
-launchApp <- function() {
-    shiny::runApp(system.file("app", package = "assignment4shiny"))
-}
